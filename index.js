f82d81e773811b8d82c6856a8886e644cc916d13
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2023-02-19 16:17:40 +0300

    add: clone cv from my rs-school repository

---
+++ index.js
@@ -0,0 +1,51 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuLines = document.querySelector('.menu-icon');
+const navigationMenu = document.querySelector('.header-nav');
+const welcomeMenuItems = document.querySelectorAll('.header-nav ul li');
+
+burgerMenu.addEventListener('click', changeStateWelcomeMenu);
+
+function changeStateWelcomeMenu() {
+  burgerMenuLines.classList.toggle('active');
+  navigationMenu.classList.toggle('mobile');
+  burgerMenu.classList.toggle('active');
+  document.body.classList.toggle('lock');
+}
+
+if (window.innerWidth <= 1024) {
+  for (let i = 0; i < welcomeMenuItems.length; i++) {
+    welcomeMenuItems[i].addEventListener('click', () => {
+      changeStateWelcomeMenu();
+    });
+  }
+}
+
+const smoothLinks = document.querySelectorAll('a[href*="#"]');
+
+smoothLinks.forEach((smoothLink) => {
+  smoothLink.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const href = this.getAttribute('href').substring(1);
+
+    const hrefOffset = document.getElementById(href).offsetTop;
+
+    setTimeout(function () {
+      window.scrollBy({
+        top: hrefOffset,
+        behavior: 'smooth',
+      });
+    }, 500);
+  });
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
